c028bca56b5d06efeb8d6e400fc4402d736c3fe8
gsingharoy <neogauravsvnit@gmail.com>
2017-10-03 23:16:46 +0200

    Working ServerApp
    Connecting all the dots and have a working ServerApp. Using actual dumped products data.

--- server/src/main/scala/com/gsingharoy/productssearch/server/ServerApp.scala
+++ server/src/main/scala/com/gsingharoy/productssearch/server/ServerApp.scala
@@ -5,16 +5,31 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server._
 import akka.stream.ActorMaterializer
+import com.gsingharoy.productssearch.server.api.handlers.ProductsSearchHandler
+import com.gsingharoy.productssearch.server.api.request.ProductsSearchRequest
+import com.gsingharoy.productssearch.server.repository.filesystem.FilesystemProductsRepository
+import com.gsingharoy.productssearch.server.repository.inmemory.InmemoryProductsRepository
+import com.typesafe.config.ConfigFactory
+import io.circe.syntax._
 
 object ServerApp extends App with Directives {
+  lazy val productsRepository = InmemoryProductsRepository(
+    FilesystemProductsRepository(appConfig).allProducts
+  )
+  lazy val productsSearchHandler = ProductsSearchHandler(productsRepository)
   implicit val system = ActorSystem("actor-system")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
+  val appConfig = ConfigFactory.load()
 
+
+  // TODO: Move this to a new route class SearchRoute
   val routes: Route = {
-    path("v1"/ "api" / "search") {
+    path("v1" / "api" / "search") {
       get {
         parameterMap { params =>
-          val resp: ResponseEntity = HttpEntity(ContentTypes.`application/json`, "{\"my_key\":\"my_value\"}")
+          val response = productsSearchHandler.fetchResponse(ProductsSearchRequest.fromQueryParams(params))
+
+          val resp: ResponseEntity = HttpEntity(ContentTypes.`application/json`, response.asJson.noSpaces)
           complete(HttpResponse(StatusCodes.OK, entity = resp))
         }
       }
