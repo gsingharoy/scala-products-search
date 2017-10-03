9cfa1eb575b66688a0f75bc012aea99b4823abab
gsingharoy <neogauravsvnit@gmail.com>
2017-10-03 16:36:48 +0200

    Test for JSON decoder
    Added tests for decoding of Zalando response to the RawProductsResponse to make sure the right attributes are mapped

---
+++ products-ingester/src/test/scala/com/gsingharoy/productsingester/services/fetcher/RawProductsResponseJSONDecoderSpec.scala
@@ -0,0 +1,35 @@
+package com.gsingharoy.productsingester.services.fetcher
+
+import io.circe.parser
+import org.scalatest.{GivenWhenThen, Matchers, WordSpec}
+
+import scala.io.Source
+
+
+class RawProductsResponseJSONDecoderSpec extends WordSpec with Matchers with GivenWhenThen {
+  "JSON Decoder" should {
+    "decode the response from zalando api to RawProductsResponse" in {
+      Given("A valid response returned from external products api")
+      val productsResponseJSON = Source.fromURL(getClass.getClassLoader.getResource("zalando-api-response.json")).mkString.stripMargin
+
+      When("I decode the response")
+      val unmarshalledRawProductsResponse = parser.parse(productsResponseJSON) match {
+        case Left(failure) => None
+        case Right(json) => json.as[RawProductsResponse].toOption
+      }
+
+      Then("the right attributes are copied to RawProductsResponse")
+      val expectecRawProductResponse = RawProductsResponse(
+        totalElements = Some(49035),
+        size = Some(2),
+        page = Some(1),
+        totalPages = Some(24518),
+        content = Seq(
+          RawProduct(id = "BU311C02R-802", modelId = "BU311C02R", name = "Boots - garda suede", brand = RawBrand("Buffalo")),
+          RawProduct(id = "HU312A00O-C11", modelId = "HU312A00O", name = "SLIMMER STADIL - Trainers - castle rock/ribbon red/bril blue", brand = RawBrand("Hummel"))
+        ))
+
+      unmarshalledRawProductsResponse should equal(Some(expectecRawProductResponse))
+    }
+  }
+}
