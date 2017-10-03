d77dcf6acec209023f6d50f05a07881405fa0147
gsingharoy <neogauravsvnit@gmail.com>
2017-10-03 19:36:24 +0200

    Basic route
    Built the basic stubbed route with akka-http for the route /v1/api/search

---
+++ server/src/main/scala/com/gsingharoy/productssearch/server/ServerApp.scala
@@ -0,0 +1,23 @@
+package com.gsingharoy.productssearch.server
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server._
+import akka.stream.ActorMaterializer
+
+object ServerApp extends App with Directives {
+  implicit val system = ActorSystem("actor-system")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  val routes: Route = {
+    path("v1"/ "api" / "search") {
+      get {
+        val resp: ResponseEntity = HttpEntity(ContentTypes.`application/json`, "{\"my_key\":\"my_value\"}")
+        complete(HttpResponse(StatusCodes.OK, entity = resp))
+      }
+    }
+  }
+
+  Http().bindAndHandle(routes, "0.0.0.0", 8002)
+}
