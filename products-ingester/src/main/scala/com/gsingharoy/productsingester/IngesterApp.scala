f415d77001a9a1ba8351f08605486e12b70c4e63
gsingharoy <neogauravsvnit@gmail.com>
2017-10-03 17:22:16 +0200

    Use ZalandoProductsFetcher in the App
    Use the ZalandoProductsFetcher fetch functionality to fetch the products in the main Objecy

--- products-ingester/src/main/scala/com/gsingharoy/productsingester/IngesterApp.scala
+++ products-ingester/src/main/scala/com/gsingharoy/productsingester/IngesterApp.scala
@@ -1,5 +1,26 @@
 package com.gsingharoy.productsingester
 
-object IngesterApp extends App {
+import com.gsingharoy.productsingester.services.fetcher.zalando.ZalandoProductsFetcher
+import com.typesafe.config.ConfigFactory
 
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object IngesterApp extends App {
+  val ec = scala.concurrent.ExecutionContext.Implicits.global
+  val appConfig = ConfigFactory.load()
+
+  val productsFuture1 = ZalandoProductsFetcher("shoes", 1, pageSize = 200)(ec).fetch
+  val productsFuture2 = ZalandoProductsFetcher("shoes", 2, pageSize = 200)(ec).fetch
+  val productsFuture3 = ZalandoProductsFetcher("shoes", 3, pageSize = 200)(ec).fetch
+
+  val productsFuture: Future[Seq[Product]] = for {
+    products1 <- productsFuture1
+    products2 <- productsFuture2
+    products3 <- productsFuture3
+  } yield (products1 ++ products2 ++ products3)
+
+
+  val products: Seq[Product] = Await.result(productsFuture, 10 second)
 }
